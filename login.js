7c521ea92ca7388af6f1d917a3f12f3c2b1a1640
Wissem <wbousdira@gmail.com>
2023-08-02 19:52:37 +0200

    Weather App Front

---
+++ login.js
@@ -0,0 +1,40 @@
+// Sign-up
+document.querySelector('#register').addEventListener('click', function () {
+	const user = {
+		name: document.querySelector('#registerName').value,
+		email: document.querySelector('#registerEmail').value,
+		password: document.querySelector('#registerPassword').value,
+	}
+
+	fetch('weather-app-back-psi.vercel.app/users/signup', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(user),
+	})
+		.then(response => response.json())
+		.then(data => {
+			if (data.result) {
+				window.location.assign('index.html');
+			}
+		});
+});
+
+// Sign-in
+document.querySelector('#connection').addEventListener('click', function () {
+	const user = {
+		email: document.querySelector('#connectionEmail').value,
+		password: document.querySelector('#connectionPassword').value,
+	}
+
+	fetch('weather-app-back-psi.vercel.app/signin', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(user),
+	})
+		.then(response => response.json())
+		.then(data => {
+			if (data.result) {
+				window.location.assign('index.html');
+			}
+		});
+});
